e3e36a22c49bb1273759975ed91452fc42f54e60
mabus <m.buscaroli@outlook.com>
2022-01-01 11:44:58 +0000

    added an example file regarding the delegate pattern.

---
+++ centralHub_delegateProtocol.playground/Contents.swift
@@ -0,0 +1,64 @@
+import UIKit
+
+// Protocol: any Class or Struct adhering to this protocol must implement a "deliver" function
+// with the same signature.
+protocol HasVan {
+    func deliver(to address: String) -> Void
+}
+
+
+// Class using the delegate pattern.
+// Needs to have a delegate var of the same type as the Protocol in order to connect to the other
+// structs and classes.
+class CentralHUB {
+    var delegate: HasVan?
+    
+    func pingDriver(to address: String) {
+        delegate?.deliver(to: address)
+    }
+    
+    func startOfDay() {
+        print("Unit opening...")
+    }
+}
+
+// Struct adhering to the protocol.
+// Needs to implement the function of the protocol (deliver).
+// Needs to "sign up" with the class that is using the delegate pattern, which has a var generally called delegate.
+// This can be done in the initializer.
+struct CarDriver:HasVan {
+    init(_ handler: CentralHUB) {
+        handler.delegate = self
+    }
+    
+    func deliver(to address: String) {
+        print("John is delivering to \(address) by car.")
+    }
+}
+
+struct BikeDriver:HasVan {
+    init(_ handler: CentralHUB) {
+        handler.delegate = self
+    }
+    
+    func deliver(to address: String) {
+        print("Mike is delivering to \(address) by bike.")
+    }
+}
+
+// TESTING
+
+let london01 = CentralHUB()
+let london02 = CentralHUB()
+
+london01.startOfDay()
+london02.startOfDay()
+
+let john = CarDriver(london01)
+let mike = BikeDriver(london02)
+
+london01.pingDriver(to: "Victor Meldrew, the Pain in the Arse, Luton")
+london02.pingDriver(to: "Roy and Moss, the Basement, Denholm Industries")
+
+london01.pingDriver(to: "John and Mary, the Shop, Craggy Island")
+london02.pingDriver(to: "Manuel, the room with a view, Fawlty Towers, Cornwall")
